c776bf7b63b5b24153088000755d6b0c474a6dde
Ramy Anber <71028466+ramyanber94@users.noreply.github.com>
2024-09-22 00:37:44 +0300

    added flowbite

---
+++ src/app/images-to-pdf/page.tsx
@@ -0,0 +1,11 @@
+import { Alert } from "flowbite-react";
+
+export default function ImagesToPdf() {
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+      {/* <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="">
+    </div> */}
+    </div>
+  );
+}
